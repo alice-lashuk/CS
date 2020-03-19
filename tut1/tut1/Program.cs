52f1984c17d98f0151f46f328d115404b1da8527
alice-lashuk <alice.lashuk@gmail.com>
2020-03-19 22:20:58 +0100

    Add tut1

---
+++ tut1/tut1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tut1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+        }
+    }
+}
